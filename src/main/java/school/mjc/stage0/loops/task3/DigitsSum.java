a1f35bcea99c4b33a67ba5d2daac21f9b403447a
nurzhan <Salikh835@gmail.com>
2023-02-18 17:50:53 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,15 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
-
+    public void printDigitsSum(int t) {
+        int digit, sum = 0;
+        while (t > 0) {
+//finds the last digit of the given number
+            digit = t % 10;
+//adds last digit to the variable sum
+            sum = sum + digit;
+//removes the last digit from the number
+            t = t / 10;
+        }
     }
 }
